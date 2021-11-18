6c88b70ba02443a4ccd9bbb4f76cadeec9efb403
Jorge Luis <cokimaniac@gmail.com>
2021-11-18 17:05:11 -0400

    RoomViewset created

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+#models
+from .models import Room
+
+#serializers
+from .serializers import RoomSerializer
+
+class RoomsViewset(ModelViewSet):
+    serializer_class = RoomSerializer
+    queryset = Room.objects.all()
+    filterset_fields = ["type"]
+    filter_backends = [DjangoFilterBackend]
