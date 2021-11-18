5da685bbcfdd400c3aa5b6ef18685b47e89bae1b
Jorge Luis <cokimaniac@gmail.com>
2021-11-18 15:29:35 -0400

    rooms app created Room model created

---
+++ rooms/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Room (models.Model):
+    TYPE_CHOICES = (
+        ("IND", "Individual"),
+        ("TWIN", "Twin"),
+        ("DOUB", "Doubble"),
+        ("TRIP", "Triple"),
+        ("QUAD", "Quadruple"),
+        ("ROOM", "Entire Room")
+    )
+
+    details = models.TextField(blank=False)
+    type = models.CharField(
+        blank=False, 
+        min_length=4, 
+        choices=TYPE_CHOICES
+    )
+    price = models.FloatField()
+
+    def __init__ (self):
+        return self.type
