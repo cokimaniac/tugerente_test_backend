f86823cfc1fbb7b95ffd5144a046c1bf34cf19ed
Jorge Luis <cokimaniac@gmail.com>
2021-11-18 23:20:34 -0400

    RoomsViewset requires authentication

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,9 @@
+from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from django_filters.rest_framework import DjangoFilterBackend
 
 #models
@@ -14,3 +17,4 @@ class RoomsViewset(ModelViewSet):
     queryset = Room.objects.all()
     filterset_fields = ["type"]
     filter_backends = [DjangoFilterBackend]
+    permission_classes = [IsAuthenticated]
