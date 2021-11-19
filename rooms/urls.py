6cb13bc7e481a0787e99dbe349a9ae3e6caeb95b
Jorge Luis <cokimaniac@gmail.com>
2021-11-18 23:19:32 -0400

    comments are removed

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,16 +4,7 @@ from rest_framework.routers import SimpleRouter
 #views
 from . import views
 
-# urlpatterns = [
-#     path(
-#         route="",
-#         name="rooms",
-#         view=views.RoomsViewset.as_view()
-#     )
-# ]
-
 urlpatterns = []
-
 
 router = SimpleRouter()
 router.register("", views.RoomsViewset)
