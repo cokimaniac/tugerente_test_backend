808cafa2d457afd6457f3b40b46b5e98eb8b7ae7
Jorge Luis <cokimaniac@gmail.com>
2021-11-18 23:18:23 -0400

    ClientManager added for authentication

--- clients/models.py
+++ clients/models.py
@@ -1,5 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class ClientManager(BaseUserManager):
+
+    def create_user (self, email, password=None):
+        if not email:
+            raise ValueError("Client user must be linked with email")
+
+        client = self.model(
+            email=self.normalize_email(email),
+        )
+        client.is_staff = False
+        client.set_password(password)
+        client.save(using=self._db)
+        return client
 
 class Client(AbstractBaseUser):
     email = models.EmailField(
@@ -22,10 +36,12 @@ class Client(AbstractBaseUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    objects = ClientManager()
     
     def get_fullname(self):
         return f"{self.first_name} {self.last_name}"
 
-    def str(self):
+    def __str__(self):
         return self.username
 
